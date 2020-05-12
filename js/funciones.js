667d7a74d62169b1151240d66311792b0b16e9a7
jaumecosta <jaumecostagomez@gmail.com>
2020-05-13 01:39:12 +0200

    ejemplos funciones

---
+++ js/funciones.js
@@ -0,0 +1,31 @@
+
+function holaMundo(){
+    return "Hola Mundo";
+}
+
+var  numero1 = 10;
+var numero2 = 30;   
+function multiplicar(numero1, numero2){
+    console.log(numero1 * numero2);
+}
+
+multiplicar(numero1, numero2)
+
+function numero(){
+    var explicito = 30 / Number("3");
+    console.log(explicito);
+}
+
+numero()
+
+
+function carta(nombre, apellido){
+    var nombre = "Jaume";
+    var apellido = "Costa";
+    console.log("Hola señor " + nombre + " " + apellido);
+}
+
+carta()
+
+var array = [4, 8, 15, 16, 23, 42]
+console.log("Numero loteria Harley " + array)
